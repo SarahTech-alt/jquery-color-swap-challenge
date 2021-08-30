738d4e4a596a2fca3595c61fbbf6eb15f44714f3
Sarah Fuoss <sfuoss@gmail.com>
2021-08-30 13:40:58 -0500

    added buttons yellow and delete

--- client.js
+++ client.js
@@ -9,5 +9,10 @@ let clickCount = 0; // initializing a variable for a counter
 function addDiv() {
     // console.log('adding div'); checking function call
     clickCount++; // add one to counter
-    $('.add-content').append(`<div class='added-div'><p>Number of clicks: ${clickCount} </p></div>`) // add a div with the click count to the DOM
+    $('.add-content').append(`
+    <div class='added-div'>
+    <p>Number of clicks: ${clickCount} </p>
+    <button class="make-yellow">Yellow</button>
+    <button class="delete-color-row">Delete</button>
+    </div>`) // add a div with the click count to the DOM
 }
