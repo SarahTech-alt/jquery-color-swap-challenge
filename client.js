f3c70608f24df7177ea729821e005c627fe575d4
Sarah Fuoss <sfuoss@gmail.com>
2021-08-30 13:30:25 -0500

    button functionality to add a div to the DOM

--- client.js
+++ client.js
@@ -1,6 +1,12 @@
 $(document).ready(readyNow);
 
 function readyNow(){
-    console.log('Document ready');
+    console.log('Document ready'); // validating sourcing
+    $('#generate-button').on('click', addDiv);
+}
+
+function addDiv() {
+    console.log('adding div');
     
+    $('.add-content').append(`<div class='added-div'>New Element</div>`)
 }
