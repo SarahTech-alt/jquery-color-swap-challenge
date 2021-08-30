00728fca59ccbf7b3091ddb07c9a1bc69a96e287
Sarah Fuoss <sfuoss@gmail.com>
2021-08-30 13:36:13 -0500

    added counter function

--- client.js
+++ client.js
@@ -4,9 +4,10 @@ function readyNow(){
     console.log('Document ready'); // validating sourcing
     $('#generate-button').on('click', addDiv);
 }
+let clickCount = 0; // initializing a variable for a counter
 
 function addDiv() {
-    console.log('adding div');
-    
-    $('.add-content').append(`<div class='added-div'>New Element</div>`)
+    // console.log('adding div'); checking function call
+    clickCount++; // add one to counter
+    $('.add-content').append(`<div class='added-div'><p>Number of clicks: ${clickCount} </p></div>`) // add a div with the click count to the DOM
 }
